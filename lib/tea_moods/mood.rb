a12d2783ca1a970f80aad501dc7bcc72a3ffd79d
Jacob Klimenko <droidedjake@gmail.com>
2020-12-10 22:47:31 -0500

    Move self.scrape_moods to Scraper class

--- lib/tea_moods/mood.rb
+++ lib/tea_moods/mood.rb
@@ -5,18 +5,7 @@ class TeaMoods::Mood
 
     def self.list
         # Scrape benefits page & return moods from that page
-        self.scrape_moods
+        # self.scrape_moods
     end
 
-    def self.scrape_moods
-        moods = []
-
-        doc = Nokogiri::HTML(open("https://www.traditionalmedicinals.com/collection/benefit-all/"))
-        mood = doc.css("div.tm-collection-benefit-section div.tm-collection-product.tm-collection-product--intro h2 span")
-        binding.pry
-        # Go to Traditional Medicinals benefits page
-        # Extract the moods
-        # Instantiate moods
-        moods
-    end
 end
